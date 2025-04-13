ee294f9139026d3a6936696445d7622412e44c23
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-04-13 20:41:38 +0900

    add

---
+++ asp_test_1.js
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+        <form name="_ctl0" method="post" action="regscript_cs.aspx" id="_ctl0">
+            <input type="hidden" name="__VIEWSTATE" value="dDwyMDA1M……utl+rWwY=" />
+            <script language="JavaScript"> alert('クライアント') </script>
+            <input name="myTextBox" type="text" id="myTextBox" />
+            <input type="submit" name="myButton" value="ボタン" id="myButton" />
+            <script language="JavaScript"> alert('スタートアップ') </script>
+        </form>
+    </body>
+</html>;
